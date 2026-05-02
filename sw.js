e1ebf8385c5d8d1c257ae2eccbbc488e24746579
easytravelcye-ship-it <easytravel.cye@gmail.com>
2026-05-02 18:39:54 -0300

    Kill SW cache definitivo

--- sw.js
+++ sw.js
@@ -1,21 +1,9 @@
-// Force clear all caches
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    ).then(() => self.skipWaiting())
-  );
-});
-
+// Minimal SW - no caching, just pass through
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    ).then(() => clients.claim())
+    caches.keys().then(k => Promise.all(k.map(c => caches.delete(c))))
+    .then(() => self.clients.claim())
   );
 });
-
-// No caching - always fetch fresh
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => new Response('', {status: 503}))));
